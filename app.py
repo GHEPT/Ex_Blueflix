238c2b664d36fdbf8dc0cd016b714e9b92092c22
GHEPT <teodoro.edu@hotmail.com>
2021-06-30 19:33:44 -0300

    Connect PostgresSQL

--- app.py
+++ app.py
@@ -1,47 +1,46 @@
 from flask import Flask, Blueprint, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 bp = Blueprint('app', __name__)#é o nome da nossa aplicação.
 
-filmes = [
-    {
-        "id": 1,
-        "nome": "Orgulho e Preconceito",
-        "imagem_url": "https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/87/84/14/20028635.jpg"
-    },
-    {
-        "id": 2,
-        "nome": "Tróia",
-        "imagem_url": "https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/90/43/26/20096298.jpg"
-    },
-    {
-        "id": 3,
-        "nome": "Príncipe da Pérsia",
-        "imagem_url": "https://upload.wikimedia.org/wikipedia/pt/7/7d/Prince_of_Persia_The_Forgotten_Sands_Capa.jpg"
-    },
-    {
-        "id": 4,
-        "nome": "A Origem",
-        "imagem_url": "https://upload.wikimedia.org/wikipedia/pt/8/84/AOrigemPoster.jpg"
-    },
-    {
-        "id": 5,
-        "nome": "Meninas Malvadas",
-        "imagem_url": "https://br.web.img3.acsta.net/pictures/210/087/21008705_20130527194056821.jpg"
-    },
-    {
-        "id": 6,
-        "nome": "Click",
-        "imagem_url": "https://images-na.ssl-images-amazon.com/images/I/51qq%2B12klKL._AC_.jpg"
-    }
-]
+# Database
+user = 'ftmeceao'
+password = 'T9lZLHP_lSMbMXIxPjAlekEtoVatjTrx'
+host = 'tuffi.db.elephantsql.com'
+database = 'ftmeceao'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}/{database}'
+# Variável que contém toda a configuração do PostgreSQL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+#Modelo Filmes (cada modelo é uma tabela)
+class Filmes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(255), nullable=False)
+    imagem_url = db.Column(db.String(255), nullable=False) 
+
+    def __init__(self, nome, imagem_url):
+        self.nome = nome
+        self.imagem_url = imagem_url
+    
+    @staticmethod
+    def read_all():
+        #Select * FROM FILMES
+        return Filmes.query.order_by(Filmes.id.desc()).all()
+
 
 @bp.route('/')#bp é de Blueprint
 def index():
     return render_template('index.html')
 
 @bp.route('/read')
-def listar_filmes():
+def read_all():
+    filmes = Filmes.read_all()
     return render_template('listar_filmes.html', filmes=filmes)#passando pra dentro do meu HTML a minha listagem de filmes. O primeiro nome eu uso dentro do HTML. O segundo nome é minha lista de dict.
 
 @bp.route('/read/<filme_id>')
